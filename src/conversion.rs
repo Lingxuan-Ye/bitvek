b6f24320fb589ab000fed4d4773fd8b3a9b3b0b8
Lingxuan Ye <love@lingxuan.io>
2024-07-03 05:54:43 +0800

    feat: implement `BitVec::from_bytes`

--- src/conversion.rs
+++ src/conversion.rs
@@ -1,4 +1,22 @@
-use super::BitVec;
+use super::{BitVec, U3};
+
+impl BitVec {
+    /// Creates a new [`BitVec`] from a slice of bytes.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use bitvek::BitVec;
+    ///
+    /// let vec = BitVec::from_bytes(&[0b1111_0000, 0b0000_1111]);
+    /// ```
+    pub fn from_bytes(bytes: &[u8]) -> Self {
+        Self {
+            data: bytes.to_vec(),
+            unused: U3(0),
+        }
+    }
+}
 
 impl<T> From<T> for BitVec
 where
