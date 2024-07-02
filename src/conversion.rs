b5458bccfa0576c8bc2794f1d4f9ffdba761f749
Lingxuan Ye <love@lingxuan.io>
2024-07-03 06:10:55 +0800

    test: add unittest for mod `conversion`

--- src/conversion.rs
+++ src/conversion.rs
@@ -38,3 +38,41 @@ impl From<&BitVec> for Vec<bool> {
         value.iter().collect()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_from_bytes() {
+        let expected = BitVec {
+            data: vec![0b1111_0000, 0b0000_1111],
+            unused: U3(0),
+        };
+
+        let vec = BitVec::from_bytes(&[0b1111_0000, 0b0000_1111]);
+        assert_eq!(vec, expected);
+    }
+
+    #[test]
+    fn test_from_bool_slice() {
+        let expected = BitVec {
+            data: vec![0b1010_0000],
+            unused: U3(4),
+        };
+
+        let vec = BitVec::from([true, false, true, false]);
+        assert_eq!(vec, expected);
+    }
+
+    #[test]
+    fn test_into_bool_vec() {
+        let bits = BitVec::from([true, false, true, false]);
+
+        let bools: Vec<bool> = (&bits).into();
+        assert_eq!(bools, vec![true, false, true, false]);
+
+        let bools: Vec<bool> = bits.into();
+        assert_eq!(bools, vec![true, false, true, false]);
+    }
+}
