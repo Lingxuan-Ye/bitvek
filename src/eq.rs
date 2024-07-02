9b75f25772f0df8bbdaacc4ae6ad453beefd14a7
Lingxuan Ye <love@lingxuan.io>
2024-07-03 06:04:30 +0800

    test: add unittest for mod `eq`

--- src/eq.rs
+++ src/eq.rs
@@ -24,3 +24,54 @@ impl PartialEq for BitVec {
 }
 
 impl Eq for BitVec {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::U3;
+
+    #[test]
+    fn test_eq() {
+        let expected = BitVec {
+            data: vec![0b1111_0000, 0b0000_1111],
+            unused: U3(2),
+        };
+
+        let vec = BitVec {
+            data: vec![0b1111_0000, 0b0000_1111],
+            unused: U3(3),
+        };
+        assert_ne!(vec, expected);
+
+        let vec = BitVec {
+            data: vec![0b1111_0000, 0b1100_1111],
+            unused: U3(2),
+        };
+        assert_ne!(vec, expected);
+
+        let vec = BitVec {
+            data: vec![0b1111_0000, 0b0000_1111, 0b1111_0000],
+            unused: U3(2),
+        };
+        assert_ne!(vec, expected);
+
+        let vec = BitVec {
+            data: vec![0b1111_0000, 0b0000_1111],
+            unused: U3(2),
+        };
+        assert_eq!(vec, expected);
+
+        let vec = BitVec {
+            data: vec![0b1111_0000, 0b0000_1100],
+            unused: U3(2),
+        };
+        assert_eq!(vec, expected);
+
+        let vec = BitVec::new();
+        assert_ne!(vec, expected);
+        assert_ne!(expected, vec);
+
+        let expected = BitVec::new();
+        assert_eq!(vec, expected);
+    }
+}
