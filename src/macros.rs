6de229b97d601ae89496a027c47a6d9fb9d8571f
Lingxuan Ye <love@lingxuan.io>
2024-07-05 19:24:44 +0800

    fix: fix a bug where calling `bitvec![]` requires type annotation

--- src/macros.rs
+++ src/macros.rs
@@ -3,11 +3,15 @@
 /// [`BitVec`]: crate::BitVec
 #[macro_export]
 macro_rules! bitvec {
+    [] => {
+        $crate::BitVec::new()
+    };
+
     [$elem:expr; $n:expr] => {
         $crate::BitVec::from([$elem; $n])
     };
 
-    [$($elem:expr),* $(,)?] => {
-        $crate::BitVec::from([$($elem,)*])
+    [$($elem:expr),+ $(,)?] => {
+        $crate::BitVec::from([$($elem,)+])
     };
 }
