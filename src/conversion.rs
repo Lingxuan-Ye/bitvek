60a0dbb557a9c65b99297ce3b3fce437dbfb8f04
Lingxuan Ye <love@lingxuan.io>
2024-07-05 18:47:49 +0800

    refactor!: remove method `BitVec::from_bytes`

--- src/conversion.rs
+++ src/conversion.rs
@@ -1,23 +1,5 @@
 use super::{BitVec, U3};
 
-impl BitVec {
-    /// Creates a new [`BitVec`] from a slice of bytes.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// use bitvek::BitVec;
-    ///
-    /// let vec = BitVec::from_bytes(&[0b1111_0000, 0b0000_1111]);
-    /// ```
-    pub fn from_bytes(bytes: &[u8]) -> Self {
-        Self {
-            data: bytes.to_vec(),
-            unused: U3(0),
-        }
-    }
-}
-
 impl<const N: usize> From<[bool; N]> for BitVec {
     fn from(value: [bool; N]) -> Self {
         value.into_iter().collect()
@@ -51,17 +33,6 @@ impl From<&BitVec> for Vec<bool> {
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn test_from_bytes() {
-        let expected = BitVec {
-            data: vec![0b1111_0000, 0b0000_1111],
-            unused: U3(0),
-        };
-
-        let vec = BitVec::from_bytes(&[0b1111_0000, 0b0000_1111]);
-        assert_eq!(vec, expected);
-    }
 
     #[test]
     fn test_from_bool_slice() {
