ccd124abeb5bcd7e2ba0980aca1c0223a638b6e3
Lingxuan Ye <love@lingxuan.io>
2024-11-10 05:46:37 +0800

    refactor: strip down src directory and refocus on new struct definitions in lib.rs

--- src/fmt.rs
+++
@@ -1,8 +0,0 @@
-use super::BitVec;
-use std::fmt::{Debug, Formatter, Result};
-
-impl Debug for BitVec {
-    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
-        f.debug_list().entries(self.iter()).finish()
-    }
-}
