a384088448ad239f6f8c0e37720b5fb731201766
Lingxuan Ye <love@lingxuan.io>
2025-11-04 01:47:08 +0800

    chore!: remove all code once again

--- src/fmt.rs
+++
@@ -1,9 +0,0 @@
-use crate::BitVec;
-use core::fmt;
-
-impl fmt::Debug for BitVec {
-    #[inline]
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.debug_list().entries(self.iter()).finish()
-    }
-}
